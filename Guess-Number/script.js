4ff92bafecae755f678acc220f6bcd097232365e
LukaKljecanin <kljecaninluka@gmail.com>
2024-12-03 12:59:46 +0100

    obrisao

--- Guess-Number/script.js
+++ Guess-Number/script.js
@@ -6,7 +6,6 @@ const secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
 let highscore = 0;
-let i;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
